6a66c9ec5a949d5c2d5a733c03e645eedc7336e4
doyoon <doyoonkim980818@gmail.com>
2022-11-23 16:41:50 +0900

    board, file_info 컬럼 정보 업데이트

--- src/main/resources/sql/file_info.sql
+++ src/main/resources/sql/file_info.sql
@@ -1,8 +1,9 @@
 CREATE TABLE file_info (
 	file_no			int(11)			AUTO_INCREMENT		PRIMARY KEY,
-	board_no		int(11)			FOREIGN KEY 		NOT NULL,
+	board_no		int(11)			NOT NULL,
 	file_origin		varchar(100)	NOT NULL,
 	file_stored		varchar(100)	NOT NULL,
 	file_size		int(11)			NOT NULL,
 	file_delete_yn	varchar(100)	NOT NULL,
+	FOREIGN KEY(board_no) REFERENCES board(board_no)
 );
